d43418f5f46c4125e5e6d537b4067a9a2455ac63
Leal31 <lealemanuel31@protonmail.com>
2024-03-02 16:37:54 -0500

    Template

--- src/main/java/com/example/ahorcado/HelloApplication.java
+++ src/main/java/com/example/ahorcado/HelloApplication.java
@@ -3,18 +3,22 @@ package com.example.ahorcado;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFx");
+        primaryStage.show();
+        VBox root = new VBox();
+        Text msg = new Text("Bienvenido a JavaFx");
+        root.getChildren().add(msg);
+        Scene scene = new Scene(root, 300, 50);
+        primaryStage.setScene(scene);
     }
 
     public static void main(String[] args) {
