b0b40e7603feb10a70d4969759ee9db3052b32fd
Leal31 <lealemanuel31@protonmail.com>
2024-03-27 10:51:00 -0500

    Palabra

--- src/main/java/com/example/ahorcado/view/alert/IAlertBox.java
+++ src/main/java/com/example/ahorcado/view/alert/IAlertBox.java
@@ -1,6 +1,6 @@
 package com.example.ahorcado.view.alert;
 
 public interface IAlertBox {
-    void ShowMenssage(String title, String header, String content);
+    String ShowMenssage(String title, String header);
 
 }
