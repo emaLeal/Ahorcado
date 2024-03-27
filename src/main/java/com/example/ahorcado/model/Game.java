a0b81416cf29d65f0be81f6f3db72235a89c5845
cristian cobo <ccobo1405@gmail.com>
2024-03-26 23:33:31 -0500

    nueva vista

---
+++ src/main/java/com/example/ahorcado/model/Game.java
@@ -0,0 +1,13 @@
+package com.example.ahorcado.model;
+
+public class Game {
+
+    private String word;
+    private int attempts;
+
+    public Game(String word) {
+        this.word = word;
+        this.attempts = 0;
+    }
+
+}
--- src/main/java/com/example/ahorcado/view/alert/AlertBox.java
+++ src/main/java/com/example/ahorcado/view/alert/AlertBox.java
@@ -4,7 +4,6 @@ import javafx.scene.control.Alert;
 
 public class AlertBox implements IAlertBox{
 
-
     @Override
     public void ShowMenssage(String title, String header, String content) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
